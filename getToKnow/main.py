a6a52d6c4cd65ebfdba6600765c7c05950174d3f
kakkarotssj <kanjeckcooldeva.005@gmail.com>
2018-01-25 23:57:41 +0530

    few methods made private

--- getToKnow/main.py
+++ getToKnow/main.py
@@ -7,7 +7,7 @@ class Calculate(object):
     def __init__(self):
         self.total_lines = 0
 
-    def checker(self, path):
+    def __checker(self, path):
         while not os.path.exists(path):
             print ("Path is invalid. Try again? Y/N")
             again_input = input()
@@ -21,9 +21,9 @@ class Calculate(object):
                 else:
                     print("Not an expected input. Try again?")
                     again_input = input()
-        self.create_structure(path)
+        self.__create_structure(path)
 
-    def create_structure(self, path):
+    def __create_structure(self, path):
         everything = os.listdir(path)
         f = 0
         while f < len(everything):
@@ -41,23 +41,24 @@ class Calculate(object):
             if os.path.isfile(file_or_dir_absname):
                 file_list.append(file_or_dir_absname)
 
-        self.process_files(file_list)
-        self.process_directory(directory_list)
+        self.__process_files(file_list)
+        self.__process_directory(directory_list)
 
-    def process_files(self, file_list):
+    def __process_files(self, file_list):
         for file_name in file_list:
             file_descriptor = open(file_name, 'r')
-            self.total_lines += self.calculate_lines(file_descriptor)
+            self.total_lines += self.__calculate_lines(file_descriptor)
 
-    def process_directory(self, directory_list):
+    def __process_directory(self, directory_list):
         while True:
             try:
                 directory_name = directory_list.pop(0)
-                self.create_structure(directory_name)
+                self.__create_structure(directory_name)
             except IndexError:
                 break
 
-    def calculate_lines(self, file_descriptor):
+    @staticmethod
+    def __calculate_lines(file_descriptor):
         count = 0
         while file_descriptor.readline():
             count += 1
@@ -65,6 +66,6 @@ class Calculate(object):
         return count
 
     def count_lines(self, path):
-        self.checker(path)
+        self.__checker(path)
 
         return self.total_lines
--- getToKnow/testing.py
+++ getToKnow/testing.py
@@ -3,7 +3,7 @@ from main import Calculate
 
 def main():
     c = Calculate()
-    lines = c.count_lines('/Users/sid/Desktop/newDesktop/getToKnow/getToKnow/testing')
+    lines = c.count_lines('/Users/sid/Desktop/newDesktop/Count-Lines-Of-Code/getToKnow/testing')
 
     print(lines)
 
